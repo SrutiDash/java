b72598d4897377cc1ce664490930c8da5ce3ef18
Sruti Dash <sruti.dash1203@gmail.com>
2025-04-14 15:50:06 +0530

    upper case lower case check

--- caseCheck.java
+++ caseCheck.java
@@ -3,8 +3,14 @@ import java.util.Scanner;
 public class caseCheck {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-        System.out.println("Enter a string : ");
+        System.out.println("Enter a character : ");
         char ch = input.next().trim().charAt(0);
-        System.out.println("value is "+ ch); 
+        // System.out.println("value is "+ ch);
+        
+        if(ch >= 'a' && ch<= 'z'){
+            System.out.println(ch + " is lower case");
+        }else{
+            System.out.println(ch + " is upper case");
+        }
     }    
 }
