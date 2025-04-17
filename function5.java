c1e489f30715e8b763fbdad6832cd0c885f01292
Sruti Dash <sruti.dash1203@gmail.com>
2025-04-17 18:30:59 +0530

    pushing code

---
+++ function5.java
@@ -0,0 +1,12 @@
+public class function5 {
+    public static void main(String[] args) {
+        
+    }
+
+    static int sum(){
+        //passing arguments
+        //pass the value of numbers when you're calling the method in main
+        
+    }
+    
+}
