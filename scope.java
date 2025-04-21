19a71a19a7b4eec2260bba2e14e8c800dcac35b0
Sruti Dash <sruti.dash1203@gmail.com>
2025-04-21 16:19:32 +0530

    if you're defining the varible over a function         you can only access that inside the function as it is a local variable defined inside a particular function

---
+++ scope.java
@@ -0,0 +1,15 @@
+public class scope {
+    public static void main(String[] args) {
+        int a = 23;
+        int b = 22;
+    }
+
+    static void random(){
+        int num = 25;
+        System.out.println(num);
+
+        // if you're defining the varible over a function
+        // you can only access that inside the function as it is a local variable defined inside a particular function
+    }
+    
+}
