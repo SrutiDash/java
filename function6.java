56a7124ca1ecd2b2e1eb7fee6065f2aefe9f3da5
Sruti Dash <sruti.dash1203@gmail.com>
2025-04-21 12:53:14 +0530

    adding user inout and passing arguments together

--- function6.java
+++ function6.java
@@ -1,9 +1,20 @@
+import java.util.Scanner;
+
 public class function6 {
     public static void main(String[] args) {
+        //for first method
         String message = myMessage();
         System.out.println("Your message is : "+message);
+        //for second method
         String personalMessage = myPersonalMessage("sruti");
         System.out.println("Your message is : "+personalMessage);
+        //for third method
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter your name : ");
+        // String name = input.nextLine();
+        String myMessage = yourMessage(input.nextLine());
+        System.out.println("Your message is : "+myMessage);
+
     }
     
     static String myMessage(){
@@ -17,6 +28,14 @@ public class function6 {
     static String myPersonalMessage(String name){
         String mess = "Hii "+name;
         return mess;
+    }
+
+    //or even after passing the argument we want to take user input we can use scanner class
+    //ex
+
+    static String yourMessage(String myName){
+        String myMessage = "Hi "+myName;
+        return myMessage;
     }
 
 }
