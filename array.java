38b17fa9b260fcd8a97239bb05951c43cd10fd13
Sruti Dash <sruti.dash1203@gmail.com>
2025-04-22 18:32:07 +0530

    commit

---
+++ array.java
@@ -0,0 +1,14 @@
+public class array {
+    public static void main(String[] args) {
+        //array is a collection of data types
+        //it can be primitive not primitive
+        //all the type of data inside an array should be same
+        
+        //syntax :
+
+        //datatype[] variable_name = new datatype[];
+        int[] rollno = new int[5]; //id we know the size of it
+        //or
+        int[] rollno2 = {1, 2, 3, 4, 5}; //if we know the roll no.s
+    }
+}
