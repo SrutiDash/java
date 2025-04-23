17ade5123e47b0ef897694cb7ca98cbbb4e066f8
Sruti Dash <sruti.dash1203@gmail.com>
2025-04-23 15:13:32 +0530

    Your message

--- arrayOfObjects.java
+++ arrayOfObjects.java
@@ -6,7 +6,7 @@ public class arrayOfObjects {
         Scanner input = new Scanner(System.in);
         String[] str = new String[5];  // we do not need to take user inout for kength as it is already defined
         System.out.println("Enter the elemets of the array : ");
-        for(int i=0;i<str.length;i++){ //using arr.lkength as length is 5 here this is the formant not only when we will be taking usee input of length
+        for(int i=0;i<str.length;i++){ //using arr.length as length is 5 here this is the formant not only when we will be taking usee input of length
             str[i] = input.next();     //taking user input of elemnts of the array
         }
         System.out.println("Elements of the array are : ");    
