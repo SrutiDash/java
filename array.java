29dfe726a5411696ee492af9594ecb9158a815be
Sruti Dash <sruti.dash1203@gmail.com>
2025-04-23 11:10:50 +0530

    heap memory and stacjk memory/primitive and non primitive/array and it's index

--- array.java
+++ array.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+
 public class array {
     public static void main(String[] args) {
         //array is a collection of data types
@@ -8,7 +10,21 @@ public class array {
 
         //datatype[] variable_name = new datatype[];
         int[] rollno = new int[5]; //id we know the size of it
+        //here new keyword is used to create an object
+        //here it will create an array of size 5 in the heap memory
         //or
+        System.out.println(rollno[0]);
+        //as nothing has been initialied inside the array of size 5
+        //there for for data type int the bydefault values in the index is 0 only.
         int[] rollno2 = {1, 2, 3, 4, 5}; //if we know the roll no.s
+        System.out.println(rollno2[3]);
+        rollno2[3] = 78;
+        System.out.println(rollno2[3]);
+        System.out.println(Arrays.toString(rollno2));
+        String[] arr = new String[7];
+        System.out.println(arr[5]);
+        //for string it is null if no specifix values  has been initualies to it
+        //primitive datatypes are being stored in stack memory lie int char boolean
+        //where as non primitive type and other objects being stored in heap memory
     }
 }
