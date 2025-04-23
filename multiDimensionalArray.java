36a6b37339e40fe079224c2b674f3e4c7cd60f97
Sruti Dash <sruti.dash1203@gmail.com>
2025-04-23 15:30:50 +0530

    how one d and two d arrays are being defined

---
+++ multiDimensionalArray.java
@@ -0,0 +1,13 @@
+public class multiDimensionalArray {
+    public static void main(String[] args) {
+        //2D array : 
+        // 1 2 3 
+        // 4 5 6 
+        // 7 8 9
+        // for one D array the array is defined like this :
+        // int[]
+        // for two D array :
+        // int[][]
+
+    }
+}
