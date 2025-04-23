d8bf717ceebc7f40ee31302921c42d7684b0f26b
Sruti Dash <sruti.dash1203@gmail.com>
2025-04-23 16:01:14 +0530

    1D array and 2D array how to print them

--- multiDimensionalArray.java
+++ multiDimensionalArray.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+
 public class multiDimensionalArray {
     public static void main(String[] args) {
         //2D array : 
@@ -9,5 +11,20 @@ public class multiDimensionalArray {
         // for two D array :
         // int[][]
 
+        int[][] arr = new int[3][];
+        //here 3 is the no. of rows, right nrxt to it is column which is not necessary to specify
+        //adding the rows is mandetory 
+        //but even if you specidy numbe rof columns, you'll still need to specify rows
+
+        //2D array : 
+        int[][] array = {
+            {1, 2, 3},   //{1, 2, 3} this is 0th index of this array
+            {4, 5, 6},   //1st
+            {7, 8, 9}    //2nd
+        };
+        //to print 1D array we need to use Arrays.toString
+        System.out.println(Arrays.toString(array[1]));
+        //so print 2D array we need to print it like this :
+        System.out.println(array[1][2]);
     }
 }
