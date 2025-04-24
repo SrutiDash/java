c8550f2478230000e664a38118b0b70ee1e8da3a
Sruti Dash <sruti.dash1203@gmail.com>
2025-04-24 17:21:26 +0530

    to get the max value in a particular range

--- maxArray.java
+++ maxArray.java
@@ -1,12 +1,24 @@
 public class maxArray {
     public static void main(String[] args) {
-        int[] arr = {1,2,3,14,5,6,7};
+        int[] arr = {17,2,3,4,15,16,7};
         System.out.println(max(arr));
+        System.out.println(maxRange(arr, 2, 5));
     }
 
     static int max(int[] arr){
         int maxValue = arr[0]; //assuming index of 0 is the max element
         for(int i=0;i<arr.length;i++){
+            if(arr[i]>maxValue){
+                maxValue = arr[i];
+            }
+        }
+        return maxValue;
+    }
+
+    //to check between a particular range
+    static int maxRange(int arr[], int start, int end){
+        int maxValue = arr[start];
+        for(int i=start;i<=end;i++){
             if(arr[i]>maxValue){
                 maxValue = arr[i];
             }
