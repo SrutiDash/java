faaa6711e35d7920d9b86ba77b759e061edab33a
Sruti Dash <sruti.dash1203@gmail.com>
2025-04-24 17:31:47 +0530

    two pointer and reverse of an array

--- ArrayListSwap.java
+++ ArrayListSwap.java
@@ -7,7 +7,22 @@ public class ArrayListSwap {
         System.out.println("Your new array is : ");
         swap(arr, 1, 3);
         System.out.println(Arrays.toString(arr));
+        reverse(arr);
+        System.out.println(Arrays.toString(arr));
     }
+
+    //two pointer method : 
+
+    static void reverse(int[] arr){
+        int start = 0;
+        int end = arr.length-1;
+        while(start<end){
+            swap(arr, start, end);
+            start++;
+            end--;
+        }
+    }
+
     static void swap(int[] arr, int index1, int index2){
         int temp = arr[index1];
         arr[index1] = arr[index2];
