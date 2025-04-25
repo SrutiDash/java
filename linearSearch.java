28ab6023dae31ba0702d5cc4d426339798689c8e
Sruti Dash <sruti.dash1203@gmail.com>
2025-04-25 12:17:59 +0530

    to get the index of the array using linear search method using functions for loop and if and else statement

--- linearSearch.java
+++ linearSearch.java
@@ -10,6 +10,30 @@ public class linearSearch {
         //space complexity : how much extra memory an algorithm uses as the 
         //input size grows.
 
+        //linear search :
 
+        int[] num = {1,2,3,4,5,6,7,8,9,0};
+        int target = 7;
+        int ans = linearSearch(num, target);
+        System.out.println("Array index of "+target+" is : ");
+        System.out.println(ans); 
+
+    }
+    
+    //search in the array : 
+    //return the index if the item is forund otherwise return -1
+    static int linearSearch(int[] arr, int target){
+        if(arr.length==0){
+            return -1;
+        }
+
+        for(int index=0;index<arr.length;index++){
+            int element = arr[index];
+            if(element==target){
+                return index;
+            }
+        }
+    
+        return -1;
     }
 }
