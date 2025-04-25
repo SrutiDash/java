31029783822cf5831c70c2eac6b5264416c44a14
Sruti Dash <sruti.dash1203@gmail.com>
2025-04-25 11:49:22 +0530

    time and space complexoty of linear search, O(n) is worst time complexity and O(1) is the best

---
+++ linearSearch.java
@@ -0,0 +1,15 @@
+public class linearSearch {
+    public static void main(String[] args) {
+        // arr = [1,2,3,4,5,6,7]
+        //if no value has been detected in the array list then return -1;
+        
+        //time complexity : how the time grows based on your input
+        //the worst case of time complexity of linear search is O(n)
+        //bext is O(1)
+
+        //space complexity : how much extra memory an algorithm uses as the 
+        //input size grows.
+
+
+    }
+}
